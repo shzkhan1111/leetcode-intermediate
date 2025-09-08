36b701113d78195affea436e71208b79b2634fd5
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-09-08 14:38:31 +0500

    Dp With Memoization Fibonacci and climbing stairs

--- leetcode basics/Program.cs
+++ leetcode basics/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.Common;
 using System.Runtime.ExceptionServices;
+using System.Security.AccessControl;
 
 public class Solution
 {
@@ -15,9 +16,10 @@ public class Solution
         }
         return FibRecursion(capacity - 2) + FibRecursion(capacity - 1);
     }
+    private static Dictionary<int, int> memo = new Dictionary<int, int>();
     public static int FibonacciMemoization(int capacity)
     {
-        Dictionary<int, int> memo = new Dictionary<int, int>();
+        
         if (capacity <= 1)
         {
             return capacity;
@@ -58,23 +60,71 @@ public class Solution
         }
         return b;
     }
-
-    public static int ClimbingStairsNoExtraSpace(int n)
+    //int ClimbStairsRecursive(int n)
+    //{
+    //    if (n <= 2) return n; // base cases
+    //    return ClimbStairsRecursive(n - 1) + ClimbStairsRecursive(n - 2);
+    //}
+    public static int ClimbingStairsRecursion(int n)
     {
-        if (n < 2)
+        if (n <= 2)
         {
             return n;
         }
-        
-        int[] data = new int[n + 1];
+        return ClimbingStairsRecursion(n - 1) + ClimbingStairsRecursion(n - 2);
+    }
+    private static Dictionary<int, int> NumbersToWaysMapping = new Dictionary<int, int>();
+    public static int ClimbingStairsMemoizedRecursion(int n)
+    {
+        //number key 
+        //number of ways to that place value
+       
+        if (n <= 2)
+        {
+            return n;
+        }
+        //I have seen this value 
+        if (NumbersToWaysMapping.ContainsKey(n))
+        {
+            return NumbersToWaysMapping[n];
+        }
+        //since the same values is being calculated again and again 
+        // if I have seen the value memoize it
+        NumbersToWaysMapping[n] = ClimbingStairsMemoizedRecursion(n - 2) + ClimbingStairsMemoizedRecursion(n - 1);
+        //and return it
+        return NumbersToWaysMapping[n];
+
+    }
+    public static int ClimbingStairsWithExtraSpace(int n)
+    {
+        if (n <= 2)
+        {
+            return n;
+        }
+        int[] data = new int[n];
         data[0] = 1;
         data[1] = 2;
 
-        for (int i = 2; i <= n; i++)
+        for (int i = 2; i < n; i++)
         {
             data[i] = data[i - 1] + data[i - 2];
         }
-        return data[n];
+        return data[n - 1];
+    }
+    public static int ClimbingStairsNoExtraSpace(int n)
+    {
+        int first = 1, second = 2;
+        if (n <= 2)
+        {
+            return n;
+        }
+        for (int i = 2; i < n; i++)
+        {
+            int current = first + second;
+            first = second;
+            second = current;
+        }
+        return second;
     }
 }
 
@@ -84,8 +134,11 @@ public class Program
     {
         //int[] weights = { 2, 3, 4 };
         //int[] values = { 3, 4, 5 };
-        int capacity = 4;
-        var ans = Solution.ClimbingStairsNoExtraSpace(capacity);
+        int capacity = 5;
+        var ans = Solution.ClimbingStairsRecursion(capacity);
+        var ans1 = Solution.ClimbingStairsNoExtraSpace(capacity);
+        var ans2 = Solution.ClimbingStairsWithExtraSpace(capacity);
+        var ans3 = Solution.ClimbingStairsMemoizedRecursion(capacity);
 
 
 
