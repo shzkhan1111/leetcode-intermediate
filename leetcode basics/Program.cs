ddaf6dbb25b7c5a9b05a6376090226cbd7ab0980
Shahzaib Ahmed Khan <shahzaib.ahmed@hyperdatacomputing.com>
2025-12-15 14:36:26 +0500

    Pending changes

--- leetcode basics/Program.cs
+++ leetcode basics/Program.cs
@@ -28,11 +28,55 @@ public class Program
 {
     public static string organizingContainers(List<List<int>> container)
     {
-        int containercount = container.Count;
+        //logic count the total count in the container 
+        //get the total count per type of balls 
+        //sort and check if there is a ball for each conatiner 
+        //get the sum per container 
+        int count = container.Count;
+        List<int> containerSum = new List<int>();
+        List<int> typeSum = new List<int>();
+        for (int i = 0; i < count; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < count; j++)
+            {
+                sum += container[i][j];
+            }
+            containerSum.Add(sum);
+        }
+        for (int i = 0; i < count; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < count; j++)
+            {
+                sum += container[j][i];
+            }
+            typeSum.Add(sum);
+        }
+        containerSum.Sort();
+        typeSum.Sort();
+        for (int i= 0; i < count; i++)
+        {
+            if (containerSum[i] != typeSum[i])
+            {
+                return "Impossible";
+            }
+        }
+        return "Possible";
+    }
 
+    static string OrganizingContainers(List<List<int>> container)
+    {
+        return "";
     }
     public static void Main()
     {
-       
+        var containers = new List<List<int>> {
+            new List<int> {0,2},
+            new List<int> {1, 1}
+            //new List<int> {2, 1 ,1},
+
+        };
+        var s = organizingContainers(containers);
     }
 }
